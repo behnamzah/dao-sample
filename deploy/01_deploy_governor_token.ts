bf1de743ad61e8c5a25cb1281a606725b8c7c3db
Behnam <behnam.zahabi@gmail.com>
2022-03-27 20:04:27 +0430

    add delegated checkpoint

--- deploy/01_deploy_governor_token.ts
+++ deploy/01_deploy_governor_token.ts
@@ -1,10 +1,13 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 
 const deployGovernanceToken: DeployFunction = async function (hre:HardhatRuntimeEnvironment) {
+
     const { getNamedAccounts, deployments, network } = hre;
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
+
     console.log("Deploy Gavernance Token ...");
 
     const governanceToken = await deploy("GovernanceToken", {
@@ -15,6 +18,19 @@ const deployGovernanceToken: DeployFunction = async function (hre:HardhatRuntime
     })
 
     log(`deploy governance token to address : ${governanceToken.address}`);
+
+    await delegate(governanceToken.address, deployer);
+    log("Delegated!");
+
+};
+
+const delegate = async ( governanceTokenAddress: string, delegatedAccount: string ) => {
+
+    const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress);
+    const txResponse = await governanceToken.delegate(delegatedAccount);
+    await txResponse.wait(1);
+
+    console.log(`Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`);
 
 }
 
