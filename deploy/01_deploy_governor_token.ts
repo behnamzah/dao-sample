f65f23118f66e0dd5a2eaf421c8354a185145f27
Behnam <behnam.zahabi@gmail.com>
2022-03-27 19:46:32 +0430

    deploy governance token

--- deploy/01_deploy_governor_token.ts
+++ deploy/01_deploy_governor_token.ts
@@ -1,8 +1,21 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
-const DeployGovernanceToken: DeployFunction = async function (hre:HardhatRuntimeEnvironment) {
-    console.log("hello world");
+const deployGovernanceToken: DeployFunction = async function (hre:HardhatRuntimeEnvironment) {
+    const { getNamedAccounts, deployments, network } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    console.log("Deploy Gavernance Token ...");
+
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+        // waitConfirmations : true
+    })
+
+    log(`deploy governance token to address : ${governanceToken.address}`);
+
 }
 
-export default DeployGovernanceToken;
+export default deployGovernanceToken;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,24 @@
 import "hardhat-deploy"
 import "@nomiclabs/hardhat-ethers"
 import "@typechain/hardhat"
+import { HardhatUserConfig } from "hardhat/config";
 
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    localhost: {
+      chainId: 31337 
+    }
+  },
   solidity: "0.8.9",
-};
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  }
+}
+
+export default config;
